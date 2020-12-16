1f5331322ec4c0ce9961f90cc09858ecc51aee68
Karim Makhloufi <karim@kazoo.agency>
2020-12-16 19:14:06 +0100

    Loading works

--- src/UserComponent/UserComponent.test.js
+++ src/UserComponent/UserComponent.test.js
@@ -3,5 +3,5 @@ import UserComponent from "./UserComponent";
 
 test("renders learn react link", () => {
   render(<UserComponent />);
-  expect(screen.getByText("Id:")).toBeInTheDocument();
+  expect(screen.getByText("Loading")).toBeInTheDocument();
 });
