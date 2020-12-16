6a7499f001e087eca432bd5dadac7affc4ee0dfc
Karim Makhloufi <karim@kazoo.agency>
2020-12-16 19:19:13 +0100

    Test working

--- src/UserComponent/UserComponent.test.js
+++ src/UserComponent/UserComponent.test.js
@@ -1,7 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import UserComponent from "./UserComponent";
 
-test("renders learn react link", () => {
+test("Loading while no user", () => {
   render(<UserComponent />);
   expect(screen.getByText("Loading")).toBeInTheDocument();
+});
+
+test("Displays email when user", () => {
+  render(
+    <UserComponent
+      user={{
+        id: 2,
+        email: "janet.weaver@reqres.in",
+        first_name: "Janet",
+        last_name: "Weaver",
+        avatar: "https://reqres.in/img/faces/2-image.jpg",
+      }}
+    />
+  );
+  expect(screen.getByText("janet.weaver@reqres.in")).toBeInTheDocument();
 });
